bd2560b4ca154ee4d8fc8e446de15a3b89b99c04
Lauri Junkkari <latenssi@gmail.com>
2017-12-07 12:21:13 +0200

    Disable block and tx writer in csvdump

--- src/callbacks/csvdump.rs
+++ src/callbacks/csvdump.rs
@@ -10,16 +10,17 @@ use errors::{OpError, OpResult};
 use blockchain::proto::tx::{Tx, TxInput, EvaluatedTxOut};
 use blockchain::parser::types::CoinType;
 use blockchain::proto::block::Block;
-use blockchain::proto::Hashed;
+// use blockchain::proto::Hashed;
 use blockchain::utils;
 
+use rustc_serialize::base64::{ToBase64,STANDARD};
 
 /// Dumps the whole blockchain into csv files
 pub struct CsvDump {
     // Each structure gets stored in a seperate csv file
     dump_folder:    PathBuf,
-    block_writer:   BufWriter<File>,
-    tx_writer:      BufWriter<File>,
+    // block_writer:   BufWriter<File>,
+    // tx_writer:      BufWriter<File>,
     txin_writer:    BufWriter<File>,
     txout_writer:   BufWriter<File>,
 
@@ -59,8 +60,8 @@ impl Callback for CsvDump {
             let cap = 4000000;
             let cb = CsvDump {
                 dump_folder:    PathBuf::from(dump_folder),
-                block_writer:   try!(CsvDump::create_writer(cap, dump_folder.join("blocks.csv.tmp"))),
-                tx_writer:      try!(CsvDump::create_writer(cap, dump_folder.join("transactions.csv.tmp"))),
+                // block_writer:   try!(CsvDump::create_writer(cap, dump_folder.join("blocks.csv.tmp"))),
+                // tx_writer:      try!(CsvDump::create_writer(cap, dump_folder.join("transactions.csv.tmp"))),
                 txin_writer:    try!(CsvDump::create_writer(cap, dump_folder.join("tx_in.csv.tmp"))),
                 txout_writer:   try!(CsvDump::create_writer(cap, dump_folder.join("tx_out.csv.tmp"))),
                 start_height: 0, end_height: 0, tx_count: 0, in_count: 0, out_count: 0
@@ -80,14 +81,14 @@ impl Callback for CsvDump {
         info!(target: "callback", "Using `csvdump` with dump folder: {} ...", &self.dump_folder.display());
     }
 
-    fn on_block(&mut self, block: Block, block_height: usize) {
+    fn on_block(&mut self, block: Block, _block_height: usize) {
         // serialize block
-        self.block_writer.write_all(block.as_csv(block_height).as_bytes()).unwrap();
+        // self.block_writer.write_all(block.as_csv(_block_height).as_bytes()).unwrap();
 
         // serialize transaction
-        let block_hash = utils::arr_to_hex_swapped(&block.header.hash);
+        // let block_hash = utils::arr_to_hex_swapped(&block.header.hash);
         for tx in block.txs {
-            self.tx_writer.write_all(tx.as_csv(&block_hash).as_bytes()).unwrap();
+            // self.tx_writer.write_all(tx.as_csv(&block_hash).as_bytes()).unwrap();
             let txid_str = utils::arr_to_hex_swapped(&tx.hash);
 
             // serialize inputs
@@ -124,57 +125,61 @@ impl Callback for CsvDump {
     }
 }
 
-impl Block {
-    #[inline]
-    fn as_csv(&self, block_height: usize) -> String {
-        // (@hash, height, version, blocksize, @hashPrev, @hashMerkleRoot, nTime, nBits, nNonce)
-        format!("{};{};{};{};{};{};{};{};{}\n",
-            &utils::arr_to_hex_swapped(&self.header.hash),
-            &block_height,
-            &self.header.value.version,
-            &self.blocksize,
-            &utils::arr_to_hex_swapped(&self.header.value.prev_hash),
-            &utils::arr_to_hex_swapped(&self.header.value.merkle_root),
-            &self.header.value.timestamp,
-            &self.header.value.bits,
-            &self.header.value.nonce)
-    }
-}
+// impl Block {
+//     #[inline]
+//     fn as_csv(&self, block_height: usize) -> String {
+//         // (@hash, height, version, blocksize, @hashPrev, @hashMerkleRoot, nTime, nBits, nNonce)
+//         format!("{};{};{};{};{};{};{};{};{}\n",
+//             &utils::arr_to_hex_swapped(&self.header.hash),
+//             &block_height,
+//             &self.header.value.version,
+//             &self.blocksize,
+//             &utils::arr_to_hex_swapped(&self.header.value.prev_hash),
+//             &utils::arr_to_hex_swapped(&self.header.value.merkle_root),
+//             &self.header.value.timestamp,
+//             &self.header.value.bits,
+//             &self.header.value.nonce)
+//     }
+// }
 
-impl Hashed<Tx> {
-    #[inline]
-    fn as_csv(&self, block_hash: &str) -> String {
-        // (@txid, @hashBlock, version, lockTime)
-        format!("{};{};{};{}\n",
-            &utils::arr_to_hex_swapped(&self.hash),
-            &block_hash,
-            &self.value.tx_version,
-            &self.value.tx_locktime)
-    }
-}
+// impl Hashed<Tx> {
+//     #[inline]
+//     fn as_csv(&self, block_hash: &str) -> String {
+//         // (@txid, @hashBlock, version, lockTime)
+//         format!("{};{};{};{}\n",
+//             &utils::arr_to_hex_swapped(&self.hash),
+//             &block_hash,
+//             &self.value.tx_version,
+//             &self.value.tx_locktime)
+//     }
+// }
 
 impl TxInput {
     #[inline]
     fn as_csv(&self, txid: &str) -> String {
         // (@txid, @hashPrevOut, indexPrevOut, scriptSig, sequence)
-        format!("{};{};{};{};{}\n",
+        // format!("{};{};{};{};{}\n",
+        format!("{};{}\n",
             &txid,
-            &utils::arr_to_hex_swapped(&self.outpoint.txid),
-            &self.outpoint.index,
-            &utils::arr_to_hex(&self.script_sig),
-            &self.seq_no)
+            // &utils::arr_to_hex_swapped(&self.outpoint.txid),
+            // &self.outpoint.index,
+            // &utils::arr_to_hex(&self.script_sig),
+            &self.script_sig.to_base64(STANDARD),
+            // &self.seq_no)
+        )
     }
 }
 
 impl EvaluatedTxOut {
     #[inline]
-    fn as_csv(&self, txid: &str, index: usize) -> String {
+    fn as_csv(&self, _txid: &str, _index: usize) -> String {
         // (@txid, indexOut, value, @scriptPubKey, address)
-        format!("{};{};{};{};{}\n",
-            &txid,
-            &index,
-            &self.out.value,
-            &utils::arr_to_hex(&self.out.script_pubkey),
+        //format!("{};{};{};{};{}\n",
+        format!("{};\n",
+            //&_txid,
+            //&_index,
+            //&self.out.value,
+            //&utils::arr_to_hex(&self.out.script_pubkey),
             &self.script.address)
     }
 }
